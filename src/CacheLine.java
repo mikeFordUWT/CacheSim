05614dc946f32844a7e69aa7929e4337e1cac453
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-06-01 12:00:08 -0700

    added constructor and getters to CacheLine

--- src/CacheLine.java
+++ src/CacheLine.java
@@ -3,4 +3,26 @@
  */
 public class CacheLine {
 
+	private int myTag;
+	private int myState;
+	
+	/*
+	 * Constructor 
+	 */
+	public CacheLine(int tag, int state){
+		myTag = tag;
+		myState = state;
+	}
+	
+	
+	public int getTag(){
+		return myTag;
+	}
+	
+	public int getState(){
+		return myState;
+	}
+	
+	
+	
 }
