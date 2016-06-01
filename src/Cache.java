c6631f709294fca2906b597162ebbbce9ae29c78
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-06-01 11:40:36 -0700

    built a couple helper methods that are currently in main

--- src/Cache.java
+++ src/Cache.java
@@ -1,4 +1,16 @@
 
 public class Cache {
 
+	
+	
+	/*
+	 * A helper method to convert decimal to hex
+	 */
+	private int convertToHex(int n){
+		String x = Integer.toHexString(n);
+		
+		return Integer.parseInt(x);
+	}
 }
+
+
