dce71d395691199a6594babb1c23e2e34517dcbe
mbadgett <m_badgett@hotmail.com>
2016-06-06 11:03:30 -0700

    Initial Setup for my branch

--- src/CacheLine.java
+++ src/CacheLine.java
@@ -6,12 +6,12 @@ public class CacheLine {
 
 
 	private int myTag;
-	private int myState;
+	private MESI myState;
 	
 	/*
 	 * Constructor 
 	 */
-	public CacheLine(int tag, int state){
+	public CacheLine(int tag, MESI state){
 		myTag = tag;
 		myState = state;
 	}
@@ -21,7 +21,7 @@ public class CacheLine {
 		return myTag;
 	}
 	
-	public int getState(){
+	public MESI getState(){
 		return myState;
 	}
 	
