a59f53c02325f2631fb93e7539f2402007eec1da
mbadgett <m_badgett@hotmail.com>
2016-06-06 18:11:36 -0700

    merged changes for 6/6/2016 Revision 4

--- src/CPU.java
+++ src/CPU.java
@@ -2,11 +2,11 @@ import java.io.File;
 import java.util.Scanner;
 
 public class CPU {
-	
+
 	// Path to the config file
 	private String cfgPath = "init.cfg";
-	
-	// Constants that will be used for handling cores and 
+
+	// Constants that will be used for handling cores and
 	private int CacheLineSize = 16;
 	private int L1Size = 32;
 	private int L1Latency = 1;
@@ -21,41 +21,41 @@ public class CPU {
 	private int L2MWriteLatency = 400;
 	private int Associativity = 1;
 	private WritePolicy Policy = WritePolicy.writeBack;
-	
+
 	private Core[] myCores;
-	
+
 	public CPU(int numCores) {
 		if(numCores < 1) {
 			throw new IllegalArgumentException("Cannot have less than one cores.");
 		}
 		myCores = new Core[numCores];
-		
+
 		readCFG();
 		instantiateCores();
 	}
-	
+
 	private void instantiateCores() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void readCFG() {
-		
+
 		File cfgFile = null;
 		Scanner inStream = null;
 		boolean Success = false;
-		
+
 		try {
-			
+
 			cfgFile = new File(cfgPath);
 			inStream = new Scanner(cfgFile);
 		} catch(Exception e) {
-			
+
 			System.out.println("At least one error occured whilst reading in the config file.\n"
 					+ "Will continue with the constants specified in the file.\n"
 					+ "See init.cfg for more information.");
 		}
-		
+
 		while(inStream.hasNextLine()) {
 			String line = inStream.nextLine();
 			if(line.length() > 0 && line.charAt(0) != '#') {
@@ -68,18 +68,18 @@ public class CPU {
 	private void setVal(String line) {
 
 		Scanner scn = new Scanner(line);
-		
+
 		String token = scn.next();
 		int val = -1;
-		
+
 		switch(token) {
-		
+
 		case "CLS":
 			try {
 				val = scn.nextInt();
 				CacheLineSize = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L1S":
@@ -87,7 +87,7 @@ public class CPU {
 				val = scn.nextInt();
 				L1Size = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L1L":
@@ -95,7 +95,7 @@ public class CPU {
 				val = scn.nextInt();
 				L1Latency = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L2S" :
@@ -103,7 +103,7 @@ public class CPU {
 				val = scn.nextInt();
 				L2Size = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L2L" :
@@ -111,7 +111,7 @@ public class CPU {
 				val = scn.nextInt();
 				L2Latency = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L3S" :
@@ -119,7 +119,7 @@ public class CPU {
 				val = scn.nextInt();
 				L3Size = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L3L" :
@@ -127,7 +127,7 @@ public class CPU {
 				val = scn.nextInt();
 				L3Latency = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L1MS" :
@@ -135,7 +135,7 @@ public class CPU {
 				val = scn.nextInt();
 				L1MSize = (int) Math.pow(2, val);
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L1ML" :
@@ -143,7 +143,7 @@ public class CPU {
 				val = scn.nextInt();
 				L1MLatency = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L2MS" :
@@ -151,7 +151,7 @@ public class CPU {
 				val = scn.nextInt();
 				L2MSize = (int) Math.pow(2, val) - L1Size;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L2MLR" :
@@ -159,7 +159,7 @@ public class CPU {
 				val = scn.nextInt();
 				L2MReadLatency = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "L2MLW" :
@@ -167,7 +167,7 @@ public class CPU {
 				val = scn.nextInt();
 				L2MWriteLatency = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "CA" :
@@ -175,7 +175,7 @@ public class CPU {
 				val = scn.nextInt();
 				Associativity = val;
 			} catch(Exception e) {
-				
+
 			}
 			break;
 		case "WP" :
@@ -190,5 +190,8 @@ public class CPU {
 			break;
  		}
 		scn.close();
+	}
+
+	public void memoryRequest(int address) {
 	}
 }
